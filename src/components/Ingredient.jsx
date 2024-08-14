1a632f75f952c4b90fdbd0d725190db12b5bc604
Lucas-Sloan <Lucasg.sloan@gmail.com>
2024-08-14 18:18:39 -0500

    Adjusted styling for desired layout

--- src/components/Ingredient.jsx
+++ src/components/Ingredient.jsx
@@ -1,7 +1,12 @@
 // src/components/Ingredient.jsx
 const Ingredient = ({ ingredient, onButtonClick, isStacked }) => {
     return (
-      <li style={{ color: ingredient.color }}>
+      <li 
+        style={{ 
+          backgroundColor: ingredient.color,
+          color: 'white'
+        }}
+      >
         {ingredient.name}
         <button onClick={onButtonClick}>
           {isStacked ? 'X' : '+'}
